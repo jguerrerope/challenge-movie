892239b4f7d1681192d378ce4c3d5eef5778e096
Jorge Guerrero <jguerreropedev@gmail.com>
2018-04-16 15:18:04 +0200

    - created splash screen, added icon launcher

--- app/src/main/java/com/jguerrerope/movies/ui/MoviesListActivity.kt
+++ app/src/main/java/com/jguerrerope/movies/ui/MoviesListActivity.kt
@@ -1,12 +1,13 @@
-package com.jguerrerope.movies
+package com.jguerrerope.movies.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.jguerrerope.movies.R
 
-class MainActivity : AppCompatActivity() {
+class MoviesListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies_list)
     }
 }
